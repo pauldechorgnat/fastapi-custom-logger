3bd19cd82935e7623b9b1541614b719e975b58ab
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2023-08-28 15:56:40 +0200

    requirements: loosening fastapi version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setup(
     name="fastapi-middleware-logger",
-    version="0.0.3",
+    version="0.0.4",
     author="Paul Déchorgnat",
     author_email="paul.dechorgnat@gmail.com",
     url="https://github.com/pauldechorgnat/fastapi-custom-logger",
